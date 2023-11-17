a4ec1a031c9e4ba9b9ce4dbb359d7c8ebb7e5c4a
Carla MS <csmsoares0@gmail.com>
2023-11-17 11:35:46 +0000

    OrderItem, many-to-many association with extra attributes PART 1

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+
 @ComponentScan("com.myapp.repositories")
 @SpringBootApplication
 public class CourseApplication {
